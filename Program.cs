3e8d61824f39ab00614d77ea88c86b2e28af8b2f
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-08-19 02:50:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity_Framework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
